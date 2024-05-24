4dbfae5b3960df79d0030640d31149d7660fa151
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-05-24 10:09:18 -0700

    refactor: ApplyFOpt -> FOptApply

--- pkg/fopt.go
+++ pkg/fopt.go
@@ -3,9 +3,9 @@ package stdlibx
 // FOpt defines functional options for type t.
 type FOpt[T any] func(t T) error
 
-// ApplyFOpt applies all functional options to type t
+// FOptApply applies all functional options to type t
 // and returns the error if any fail to apply.
-func ApplyFOpt[T any](t T, options ...FOpt[T]) (T, error) {
+func FOptApply[T any](t T, options ...FOpt[T]) (T, error) {
 	for _, o := range options {
 		if err := o(t); err != nil {
 			return t, err
