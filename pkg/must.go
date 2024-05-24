6823a01f3dc318179f362f64e4b4230a912d8358
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-05-24 12:22:37 -0700

    feat: add Zeroer interface for custom type support

--- pkg/must.go
+++ pkg/must.go
@@ -2,16 +2,8 @@ package stdlibx
 
 import "fmt"
 
-// Must panics if given value is nil.
+// Must panics if given value is equal to the zero value fo the type.
 func Must[T any](t T) T {
-	if t == nil {
-		panic(fmt.Sprintf("Must[%T] received nil value", t))
-	}
-	return t
-}
-
-// MustNonZero panics if given value is "zero" value for type t.
-func MustNonZero[T any](t T) T {
 	if IsZero[T](t) {
 		panic(fmt.Sprintf("Must[%T] received zero value", t))
 	}
--- pkg/zero.go
+++ pkg/zero.go
@@ -2,8 +2,20 @@ package stdlibx
 
 import "reflect"
 
+// Zeroer describes types that support `IsZero` checks.
+type Zeroer interface {
+	// IsZero returns true if the value of the instance is equal
+	// to the "zero" value of the type.
+	IsZero() bool
+}
+
 // IsZero returns true if the given value is equal to the
 // zero value of the type.
 func IsZero[T any](t T) bool {
-	return reflect.DeepEqual(t, *new(T))
+	switch x := any(t).(type) {
+	case Zeroer:
+		return x.IsZero()
+	default:
+		return reflect.DeepEqual(t, *new(T))
+	}
 }
