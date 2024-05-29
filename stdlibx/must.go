849aa7c84dda1d0f96035d844de4c079435942e6
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-05-29 09:41:57 -0700

    feat: add MustE

--- stdlibx/must.go
+++ stdlibx/must.go
@@ -1,11 +1,24 @@
 package stdlibx
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// Must panics if given value is equal to the zero value fo the type.
+// Must panics if given value is equal to the zero value of the type.
 func Must[T any](t T) T {
 	if IsZero[T](t) {
 		panic(fmt.Sprintf("Must[%T] received zero value", t))
 	}
 	return t
+}
+
+// MustE panics if the given func returns an error for the value returned
+// is equal to the zero value of the type.
+func MustE[T any](fn func() (T, error)) T {
+	t, err := fn()
+	if err != nil {
+		panic(errors.Join(fmt.Errorf("MustE[%T] func returned error", *new(T)), err))
+	}
+	return Must[T](t)
 }
