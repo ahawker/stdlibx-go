f8f306510270b37e64048192764d1f08ef173186
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-06-17 11:57:05 -0700

    feat: support errors from builder pattern

--- stdlib/builder.go
+++ stdlib/builder.go
@@ -1,13 +1,17 @@
 package stdlib
 
 // Builder defines functional builder for type t.
-type Builder[T any] func(t T) T
+type Builder[T any] func(t T) (T, error)
 
 // Build applies all functional builders to type t
 // and returns the type built.
 func Build[T any](t T, builders ...Builder[T]) (T, error) {
+	var err error
 	for _, b := range builders {
-		t = b(t)
+		t, err = b(t)
+		if err != nil {
+			return t, err
+		}
 	}
 	return t, nil
 }
