fe5ec3f9d76fa0bedfa40a82256e84643ee1f397
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-06-19 15:40:15 -0700

    feat: separate closer and defer fn (deprecate in the future)

--- stdlib/defer.go
+++ stdlib/defer.go
@@ -1,5 +1,7 @@
 package stdlib
 
+import "io"
+
 // Defer is a helper for capturing errors from calls inside a 'defer'.
 func Defer(err *error, errs ...error) {
 	if err == nil {
@@ -8,9 +10,18 @@ func Defer(err *error, errs ...error) {
 	*err = ErrorJoin(*err, errs...).ErrorOrNil()
 }
 
-// DeferCloser is a helper for deferring a function call (closer) that can return an error
+// DeferCloser is a helper for deferring a io.Closer that can return an error
 // when in a function context that can return multiple errors.
-func DeferCloser(err *error, fn func() error) {
+func DeferCloser(err *error, closer io.Closer) {
+	if err == nil {
+		*err = Error{}
+	}
+	*err = ErrorJoin(*err, closer.Close()).ErrorOrNil()
+}
+
+// DeferCall is a helper for deferring a function call (closer) that can return an error
+// when in a function context that can return multiple errors.
+func DeferCall(err *error, fn func() error) {
 	if err == nil {
 		*err = Error{}
 	}
