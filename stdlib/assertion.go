cf065a89db2a974fd2b4405625a7d9682c185d4b
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-06-24 08:57:13 -0700

    feat: add type assertion helper

---
+++ stdlib/assertion.go
@@ -0,0 +1,22 @@
+package stdlib
+
+import (
+	"reflect"
+)
+
+// ErrTypeAssertionFailed is returned when attempting to type assert a value
+// that cannot be converted.
+var ErrTypeAssertionFailed = Error{
+	Code:      "type_assertion_failed",
+	Message:   "Cannot type assert value",
+	Namespace: ErrorNamespaceDefault,
+}
+
+// As performs a type assertion of 'any' value to type T. If it fails an error is returned.
+func As[T any](v any) (T, error) {
+	t, ok := v.(T)
+	if !ok {
+		return *new(T), ErrTypeAssertionFailed.Wrapf("value_type=%T desired_type=%s", v, reflect.TypeFor[T]())
+	}
+	return t, nil
+}
