c25a745e413c7874d04dc2602b098740c98f0c98
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-06-27 13:47:38 -0700

    feat: add MustXYZ helpers for type conversion

--- stdlib/must.go
+++ stdlib/must.go
@@ -3,6 +3,8 @@ package stdlib
 import (
 	"fmt"
 	"reflect"
+	"strconv"
+	"time"
 )
 
 // Must panics if given value is equal to the zero value of the type.
@@ -30,4 +32,74 @@ func MustE[T any](fn func() (T, error)) T {
 		panic(ErrorJoin(fmt.Errorf("MustE[%s] func returned error", reflect.TypeFor[T]()), err))
 	}
 	return Must[T](t)
+}
+
+// MustString returns the string representation of the given value and panics if it cannot.
+func MustString[T any](value T) string {
+	switch v := any(value).(type) {
+	case string:
+		return v
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+// MustBool returns the bool representation of the given value and panics if it cannot.
+func MustBool[T any](value T) bool {
+	switch v := any(value).(type) {
+	case bool:
+		return v
+	default:
+		return !IsZero[T](value)
+	}
+}
+
+// MustInt returns the int representation of the given value and panics if it cannot.
+func MustInt[T any](value T) int {
+	switch v := any(value).(type) {
+	case int:
+		return v
+	case float32:
+		return int(v)
+	case float64:
+		return int(v)
+	case int32:
+		return int(v)
+	case int64:
+		return int(v)
+	case int8:
+		return int(v)
+	case int16:
+		return int(v)
+	case uint:
+		return int(v)
+	case uint8:
+		return int(v)
+	case uint16:
+		return int(v)
+	case string:
+		i, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		return int(i)
+	default:
+		panic(fmt.Sprintf("cannot convert %T to int", value))
+	}
+}
+
+// MustDuration returns the time.Duration representation of the given value and panics if it cannot.
+func MustDuration[T any](value T) time.Duration {
+	switch v := any(value).(type) {
+	case time.Duration:
+		return v
+	case string:
+		d, err := time.ParseDuration(v)
+		if err != nil {
+			panic(err)
+		}
+		return d
+	default:
+		panic(fmt.Sprintf("cannot convert %T to time.Duration", value))
+	}
 }
