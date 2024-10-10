0370e01ec92a9df7f10a4ddd373ef3dbf5dfe123
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-10-10 10:40:42 -0700

    feat: add ZeroPad

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/ahawker/stdlibx-go
 
-go 1.21.5
+go 1.22.0
 
-require golang.org/x/text v0.14.0
+toolchain go1.22.1
+
+require (
+	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
+	golang.org/x/text v0.14.0
+)
--- stdlib/strings.go
+++ stdlib/strings.go
@@ -1,6 +1,8 @@
 package stdlib
 
 import (
+	"fmt"
+	"golang.org/x/exp/constraints"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
@@ -10,4 +12,9 @@ import (
 // Ref: https://en.wikipedia.org/wiki/Title_case
 func TitleCase(s string) string {
 	return cases.Title(language.English, cases.Compact).String(s)
+}
+
+// ZeroPad returns the given integer as a zero-padded string.
+func ZeroPad[T constraints.Integer](width T, v T) string {
+	return fmt.Sprintf("%0*d", width, v)
 }
