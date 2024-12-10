761f8b6c3fe931332bdd6ee71e04bea5f8ae637d
Andrew Hawker <andrew.r.hawker@gmail.com>
2024-12-09 21:42:34 -0800

    feat: add pointer deref helpers

--- stdlib/pointer.go
+++ stdlib/pointer.go
@@ -4,3 +4,16 @@ package stdlib
 func Pointer[T any](t T) *T {
 	return &t
 }
+
+// Dereference dereference the given pointer.
+func Dereference[T any](t *T) T {
+	return *t
+}
+
+// SafeDereference dereference the given pointer and return the zero value if the pointer is nil.
+func SafeDereference[T any](t *T) T {
+	if t == nil {
+		return *new(T)
+	}
+	return *t
+}
